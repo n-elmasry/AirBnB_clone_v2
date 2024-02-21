9a46dcbe9dfbb8b3b3394467ffd1969cc93578be
n-elmasry <almassrynada411@gmail.com>
2024-02-21 21:09:24 +0200

    task9

--- models/amenity.py
+++ models/amenity.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
-from sqlalchemy import Table, Column, String
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from models.place import place_amenity
 
 
-class Amenity(BaseModel):
-    name = ""
+class Amenity(BaseModel, Base):
+    """Amenity inherits from BaseModel and Base"""
+    __tablename__ = "amenities"
+    name = Column(String(128), nullable=False)
+    place_amenities = relationship(
+        'Place', secondary=place_amenity, backref='amenities')
--- models/place.py
+++ models/place.py
@@ -3,9 +3,22 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, Integer, ForeignKey, Float, Table
 from sqlalchemy.orm import relationship
-from models.amenity import Amenity
 from models.review import Review
 import models
+
+
+place_amenity = Table('place_amenity',
+                      Base.metadata,
+                      Column('place_id',
+                             String(60),
+                             ForeignKey('places.id'),
+                             nullable=False,
+                             primary_key=True),
+                      Column('amenity_id',
+                             String(60),
+                             ForeignKey('amenities.id'),
+                             nullable=False,
+                             primary_key=True))
 
 
 class Place(BaseModel, Base):
@@ -21,3 +34,36 @@ class Place(BaseModel, Base):
     price_by_night = Column(Integer, default=0, nullable=False)
     latitude = Column(Float)
     longitude = Column(Float)
+    amenity_ids = []
+    amenities = relationship('Amenity',
+                             secondary=place_amenity,
+                             backref='places',
+                             viewonly=False)
+
+    reviews = relationship("Review",
+                           cascade="all, delete",
+                           backref="place")
+
+    @property
+    def reviews(self):
+        """ getter for reviews associated with this Place """
+        list_reviews = []
+        __objects = models.storage.all(Review)
+        for k, obj in __objects.items():
+            if place_id in obj and obj.place_id == self.id:
+                list_reviews += obj
+        return list_reviews
+
+    @property
+    def amenities(self):
+        """the getter"""
+        return self.__amenity_ids
+
+    @amenities.setter
+    def amenities(self, amenity_ids):
+        """the setter"""
+        amenity_list = []
+        amen = models.storage.all(Amenity)
+        for amenity in amen.values():
+            if amenity.place_id == self.id:
+                amenity_ids.append(amenity.id)
