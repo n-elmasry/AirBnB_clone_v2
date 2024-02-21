7a372813e794e66c113a408a295a5f199d345dc8
n-elmasry <almassrynada411@gmail.com>
2024-02-21 17:49:00 +0200

    task7

--- models/user.py
+++ models/user.py
@@ -5,9 +5,10 @@ from sqlalchemy import Column, String, Integer
 from sqlalchemy.orm import relationship
 
 
-class User(BaseModel):
+class User(BaseModel, Base):
     """This class defines a user by various attributes"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+    __tablename__ = "users"
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
