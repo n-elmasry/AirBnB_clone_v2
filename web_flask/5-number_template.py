d34eafe9276822e83b500bcb0be7a197d24e8217
n-elmasry <almassrynada411@gmail.com>
2024-05-18 21:22:03 +0500

    task 5

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -35,7 +35,7 @@ def is_number(n):
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def html_number(n):
-    return (render_template('5-number.html', n=n))
+    return render_template('5-number.html', n=n)
 
 
 if __name__ == "__main__":
