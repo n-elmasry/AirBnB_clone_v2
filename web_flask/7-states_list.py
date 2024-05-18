8962ed287f0476c41428dfbf25cbf1f7333dfc67
n-elmasry <almassrynada411@gmail.com>
2024-05-19 02:00:59 +0500

    task 7

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,26 @@
+#!/usr/tmp/python3
+"""starts a Flask web application"""
+from flask import Flask, render_template, request
+from models import storage
+from models.state import State
+from operator import attrgetter
+
+
+app = Flask(__name__)
+
+
+@app.route("/states_list", strict_slashes=False)
+def state_list():
+    all_states = list(storage.all(State).values())
+    sorted_states = sorted(all_states, key=attrgetter('name'))
+    return render_template("7-states_list.html", states=sorted_states)
+
+
+@app.teardown_appcontext
+def teardown_db(self):
+    """call close method"""
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
